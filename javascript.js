57ff9ecfe60a94857099e5822cf17209229dc1af
VorahPong <vorahpongmean@gmail.com>
2024-07-13 23:06:44 -0500

    Added function getHumanChoice to javascript.js
    The getHumanChoice() function take input from user and make sure it a valid input of "rock", "paper", or "scissor".

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 
 /*
-    This function generate a random value of "rock" "paper" or "scissors".
+    This function generate a random value of "rock" "paper" or "scissor".
 */
 function getComputerChoice(){
     let rand = Math.floor(Math.random() * 3) //Generate between number 0-2
@@ -13,4 +13,19 @@ function getComputerChoice(){
     else {
         return "scissor"
     }
+}
+
+/*
+    This function get a valid choice from the user for "rock" "paper" or "scissor".
+*/
+function getHumanChoice(){
+    let choice = prompt('Enter your choice: rock, paper, or scissor')
+    choice = choice.toLowerCase();
+
+
+    while((choice === 'rock' || choice === 'paper' || choice === 'scissor') === false){
+        choice = prompt('Invalid choice. Enter your choice: rock, paper, or scissor')
+        choice = choice.toLocaleLowerCase();
+    }
+    return choice
 }
