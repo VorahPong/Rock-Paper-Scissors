fa21e158a5255fa946311111dc913e8d10850e7a
VorahPong <vorahpongmean@gmail.com>
2024-07-13 22:47:34 -0500

    Added function getComputerChoice to javascript.js
    getComputerChoice() function generate a random choice that return "rock", "paper", or "scissor".

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-alert("YO YO")
+
+/*
+    This function generate a random value of "rock" "paper" or "scissors".
+*/
+function getComputerChoice(){
+    let rand = Math.floor(Math.random() * 3) //Generate between number 0-2
+    if(rand === 0){
+        return "rock"
+    }
+    else if(rand === 1){
+        return "paper"
+    }
+    else {
+        return "scissor"
+    }
+}
